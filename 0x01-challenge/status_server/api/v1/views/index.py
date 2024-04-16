3dd2544311fc66bec415f210aa675a1b04b28f78
elsaaeid <saidsadaoy@gmail.com>
2024-04-16 15:00:04 -0700

    deploy changes

--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -8,6 +8,5 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """ Status of the web server
-    """
+    """ Status of the web server """
     return jsonify({"status": "OK"})
