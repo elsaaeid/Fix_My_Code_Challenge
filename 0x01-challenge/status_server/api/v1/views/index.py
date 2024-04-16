a2f961ba52ada1190f4c0e1a5aed21447ceab4f4
elsaaeid <saidsadaoy@gmail.com>
2024-04-16 16:17:22 -0700

    deploy changes

--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -3,4 +3,11 @@
 """
 from flask import jsonify
 
-from api.v1.views import *
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Status of the web server
+    """
+    return jsonify({"status": "OK"})
